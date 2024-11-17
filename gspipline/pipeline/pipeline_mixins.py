d36c918d7b57966723670760ee7e7b68d3b9c619
WZY99 <lhwzy99@163.com>
2024-11-17 19:26:32 +0800

    modify gs d pipeline

--- gspipline/pipeline/pipeline_mixins.py
+++ gspipline/pipeline/pipeline_mixins.py
@@ -1,5 +1,5 @@
 
-class LogMaxin:
+class LogMixin:
     def log_loss(self, losses: dict, prefix: str = "") -> None:
         for k, v in losses.items():
             self.log(f"{prefix}/{k}", v, on_step=True, on_epoch=True, prog_bar=True)
